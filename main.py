cf6bedcb430359912cd801831440bd643583ce9e
Christian Kohlstedde <christian@kohlsted.de>
2019-07-11 15:54:37 +0200

    Finish dsl exporter

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import asyncio
 import hashlib
-import logging
 import random
 import re
+import subprocess
 import time
 from http.cookies import SimpleCookie
 
 import aiohttp
 import dirtyjson as json
 from prometheus_async import aio
-from prometheus_client import Summary, Counter
-
-logging.basicConfig(level=logging.DEBUG)
+from prometheus_client import Counter, Gauge, Info, Summary
 
 from settings import _speedport, _password
+
+info = Info('speedport_exporter', 'Version information about the speedport exporter')
+info.info({
+    'version': subprocess.run(['git', 'describe', '--always'], capture_output=True).stdout.decode().strip()
+})
 
 
 class SpeedportClient:
@@ -87,16 +90,15 @@ class SpeedportClient:
         }
 
         async with self._session.post(
-                'http://{}/data/Login.json'.format(self._host),
-                data=request_data,
-                headers=headers
+            'http://{}/data/Login.json'.format(self._host),
+            data=request_data,
+            headers=headers
         ) as resp:
             content = await resp.read()
             parsed = json.loads(content)
             data = self.parse_typed_dict(parsed)
 
             assert data['login'] == 'success'
-            print(data)
 
         derivedk = hashlib.pbkdf2_hmac(
             'sha1',
@@ -132,8 +134,8 @@ class SpeedportClient:
             '_rand': random.randint(1, 1000)
         }
         async with self._session.get(
-                'http://{}/data/heartbeat.json'.format(self._host),
-                params=params
+            'http://{}/data/heartbeat.json'.format(self._host),
+            params=params
         ) as resp:
             assert resp.status == 200
             raw_data = json.loads(await resp.text())
@@ -153,19 +155,192 @@ class SpeedportClient:
 
 
 class SpeedportCollector:
+    METRICS_NAMESPACE = 'speedport'
+    METRICS_SUBSYSTEM = ''
+
     def __init__(self, client: SpeedportClient):
         self._client = client
 
+        self._collect_duration = Summary(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='collection_duration',
+            unit='seconds',
+            documentation='Duration to collect dsl metrics'
+        )
+
+    async def collect(self):
+        return await aio.time(self._collect_duration, self._collect())
+
+    # noinspection PyMethodMayBeStatic
+    async def _collect(self):
+        return []
+
+
+class SpeedportDslCollector(SpeedportCollector):
+    METRICS_SUBSYSTEM = 'dsl'
+
+    def __init__(self, client: SpeedportClient):
+        super(SpeedportDslCollector, self).__init__(client)
+
+        self._connection_info = Info(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='connection',
+            documentation='Connection information',
+        )
+
+        self._actual_data_rate = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='actual_data_rate',
+            unit='kbps',
+            documentation='Actual data rate',
+            labelnames=['direction']
+        )
+        self._attainable_data_rate = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='attainable_data_rate',
+            unit='kbps',
+            documentation='Attainable data rate',
+            labelnames=['direction']
+        )
+        self._snr = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='snr',
+            documentation='SNR Margin',
+            labelnames=['direction']
+        )
+        self._signal = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='signal',
+            documentation='Signal level',
+            labelnames=['direction']
+        )
+        self._line = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='line',
+            documentation='Line Attenuation',
+            labelnames=['direction']
+        )
+        self._fec_size = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='fec_size',
+            documentation='FEC Size',
+            labelnames=['direction']
+        )
+        self._codeword = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='codeword_size',
+            documentation='Codeword size',
+            labelnames=['direction']
+        )
+        self._interleave = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='interleave',
+            documentation='Interleave delay',
+            labelnames=['direction']
+        )
+        self._crc_error_count = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='crc_error_count',
+            documentation='CRC (Cyclic Redundancy Check) error count',
+            labelnames=['direction']
+        )
+        self._hec_error_count = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='hec_error_count',
+            documentation='HEC (Header Error Correction) error count',
+            labelnames=['direction']
+        )
+        self._fec_error_count = Gauge(
+            namespace=self.METRICS_NAMESPACE,
+            subsystem=self.METRICS_SUBSYSTEM,
+            name='fec_error_count',
+            documentation='FEC (Forward Error Correction) error count',
+            labelnames=['direction']
+        )
+
+    async def _collect(self):
+        data = await self._client.fetch_data('dsl')
+        connection = data['Connection']
+        line = data['Line']
+
+        self._connection_info.info(connection)
+
+        self._actual_data_rate.labels('upload').set(line['uactual'])
+        self._actual_data_rate.labels('download').set(line['dactual'])
+
+        self._attainable_data_rate.labels('upload').set(line['uattainable'])
+        self._attainable_data_rate.labels('download').set(line['dattainable'])
+
+        self._snr.labels('upload').set(line['uSNR'])
+        self._snr.labels('download').set(line['dSNR'])
+
+        self._signal.labels('upload').set(line['uSignal'])
+        self._signal.labels('download').set(line['dSignal'])
+
+        self._line.labels('upload').set(line['uLine'])
+        self._line.labels('download').set(line['dLine'])
+
+        self._fec_size.labels('upload').set(line['uFEC_size'])
+        self._fec_size.labels('download').set(line['dFEC_size'])
+
+        self._codeword.labels('upload').set(line['uCodeword'])
+        self._codeword.labels('download').set(line['dCodeword'])
+
+        self._interleave.labels('upload').set(line['uInterleave'])
+        self._interleave.labels('download').set(line['dInterleave'])
+
+        self._crc_error_count.labels('upload').set(line['uCRC'])
+        self._crc_error_count.labels('download').set(line['dCRC'])
+
+        self._hec_error_count.labels('upload').set(line['uHEC'])
+        self._hec_error_count.labels('downloads').set(line['dHEC'])
+
+        self._fec_error_count.labels('upload').set(line['uFEC'])
+        self._fec_error_count.labels('download').set(line['dFEC'])
+
+
+async_collectors = []
+server_stats_save = aio.web.server_stats
+
+
+async def server_stats(*args, **kwargs):
+    wait = []
+    for collector in async_collectors:
+        wait.append(asyncio.create_task(collector.collect()))
+
+    await asyncio.wait(wait)
+
+    return await server_stats_save(*args, **kwargs)
+
+
+aio.web.server_stats = server_stats
+
 
 async def main():
-    session = aiohttp.ClientSession()
-    client = SpeedportClient(_speedport, _password, session)
-    login_task = asyncio.create_task(client.login_loop())
+    async with aiohttp.ClientSession() as session:
+        client = SpeedportClient(_speedport, _password, session)
+        await client.login()
 
-    await aio.web.start_http_server(port=9999)
-    await login_task
+        login_task = asyncio.create_task(client.login_loop())
+
+        dsl = SpeedportDslCollector(client)
+        async_collectors.append(dsl)
+
+        await aio.web.start_http_server(port=9999)
+        await login_task
 
 
 loop = asyncio.get_event_loop()
-loop.set_debug(True)
 loop.run_until_complete(main())
